3b06cb03fa3c0a1e7563a9c33cec3e3d523e5fe2
SAM BRUTON <samwbru@gmail.com>
2025-08-20 10:59:07 +0100

    Code formatting

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -15,111 +15,114 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  items: {
-    type: Array
-  }
-})
+  const props = defineProps({
+    items: {
+      type: Array
+    }
+  })
+
+  defineEmits(['remove'])
 </script>
 
 <style scoped>
-.container {
-  position: relative;
-  margin-top: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 0;
-  list-style: none;
-  width: 60%;
-}
-
-li {
-  position: relative;
-  margin: 8px 8px 0 0;
-  padding: 8px;
-  width: fit-content;
-  border-radius: 13px;
-  //outline: 1px solid var(--vt-c-white-soft);
-  background: hsla(160, 100%, 37%, 1);
-  color: var(--vt-c-black);
-  text-transform: capitalize;
-  font-family: monospace;
-  transition: 1s all ease-in-out;
-  cursor: default;
-
-  .content-wrapper {
+  .container {
     position: relative;
-    overflow: hidden;
+    margin-top: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0;
+    list-style: none;
+    width: 60%;
   }
 
-  &:hover button.remove {
-    left: 0;
-    opacity: 1;
-  }
-
-  &:hover .fancy-border {
-    opacity: 1;
-  }
-  .fancy-border {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: '';
-    border: 2px solid greenyellow;
-    width: 100%;
-    height: 100%;
+  li {
+    position: relative;
+    margin: 8px 8px 0 0;
     padding: 8px;
-    border-radius: inherit;
-    animation: animate 2s linear infinite;
-    transition: .5s opacity ease;
-  }
+    width: fit-content;
+    border-radius: 13px;
+    //outline: 1px solid var(--vt-c-white-soft);
+    background: hsla(160, 100%, 37%, 1);
+    color: var(--vt-c-black);
+    text-transform: capitalize;
+    font-family: monospace;
+    transition: 1s all ease-in-out;
+    cursor: default;
 
-  @keyframes animate {
-    50% {
-      filter: hue-rotate(350deg);
+    .content-wrapper {
+      position: relative;
+      overflow: hidden;
     }
 
+    &:hover button.remove {
+      left: 0;
+      opacity: 1;
+    }
+
+    &:hover .fancy-border {
+      opacity: 1;
+    }
+
+    .fancy-border {
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      left: 0;
+      content: '';
+      border: 2px solid greenyellow;
+      width: 100%;
+      height: 100%;
+      padding: 8px;
+      border-radius: inherit;
+      animation: animate 2s linear infinite;
+      transition: .5s opacity ease;
+    }
+
+    @keyframes animate {
+      50% {
+        filter: hue-rotate(350deg);
+      }
+
+    }
   }
-}
 
-ul:has(li:hover) li:not(:hover) {
-  opacity: 0.2;
-}
+  ul:has(li:hover) li:not(:hover) {
+    opacity: 0.2;
+  }
 
 
-/* 1. declare transition */
-.fade-move,
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
-}
+  /* 1. declare transition */
+  .fade-move,
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  }
 
-/* 2. declare enter from and leave to state */
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  transform: scaleY(0.01) translate(30px, 0);
-}
+  /* 2. declare enter from and leave to state */
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    transform: scaleY(0.01) translate(30px, 0);
+  }
 
-/* 3. ensure leaving items are taken out of layout flow so that moving
-      animations can be calculated correctly. */
-.fade-leave-active {
-  position: absolute;
-}
+  /* 3. ensure leaving items are taken out of layout flow so that moving
+        animations can be calculated correctly. */
+  .fade-leave-active {
+    position: absolute;
+  }
 
-button.remove {
-  position: absolute;
-  appearance: none;
-  background: none;
-  border: 0;
-  position: relative;
-  left: 40px;
-  background: teal;
-  color: var(--color-text);
-  opacity: 0;
-  transition: .3s all ease;
-  cursor: pointer;
-}
+  button.remove {
+    position: absolute;
+    appearance: none;
+    background: none;
+    border: 0;
+    position: relative;
+    left: 40px;
+    background: teal;
+    color: var(--color-text);
+    opacity: 0;
+    transition: .3s all ease;
+    cursor: pointer;
+  }
 
 </style>
--- src/components/ListWithControls.vue
+++ src/components/ListWithControls.vue
@@ -61,152 +61,152 @@
 </template>
 
 <script setup>
-// TODO: Change sort button to select dropdown with 2 options; Alphabetical, Alphabet Reversed
-import ListItems from "@/components/ListItems.vue"
-import { shuffle as _shuffle } from 'lodash-es'
-import { computed, ref } from "vue"
+  // TODO: Change sort button to select dropdown with 2 options; Alphabetical, Alphabet Reversed
+  import ListItems from "@/components/ListItems.vue"
+  import { shuffle as _shuffle } from 'lodash-es'
+  import { computed, ref } from "vue"
 
-const inputVal = ref('')
+  const inputVal = ref('')
 
-const items = ref([])
-const savedLists = ref({});
-retrieveLocalStorageLists();
-
-/**
- * Get all the saved lists from local storage and return them as an array of objects
- * @returns {Array} - Array of objects with the list name and the list
- */
-function retrieveLocalStorageLists() {
-  // filter out of local storage all items that start with `list-` and have a length greater than 2 to avoid empty lists
-  // such as, `[]`.
-  const listNames = Object.keys(localStorage).filter(key => key.startsWith('list-') && localStorage.getItem(key).length > 2)
-
-  savedLists.value = listNames.map(name => {
-    const list = localStorage.getItem(name)
-    return {
-      name: name.replace('list-', ''),
-      data: JSON.parse(list)
-    }
-  })
-}
-
-function inputChange(e) {
-  const keyCode = e.code
-  switch (keyCode) {
-    case 'Enter':
-      if (!!inputVal.value) {
-        items.value.push(inputVal.value.toLowerCase())
-        // items.value.reverse()
-        inputVal.value = ''
-      }
-      break;
-  }
-}
-
-function clearList() {
-  items.value = []
-}
-
-/**
- * Save the list items to local storage, prompting the user to give the list a name
- * @returns {void}
- */
-function saveList() {
-  const list = JSON.stringify(items.value)
-  // prompt user with alert to give the list a name
-  const listName = prompt('Please enter a name for the list', 'Team Red')
-  localStorage.setItem(`list-${listName}`, list)
+  const items = ref([])
+  const savedLists = ref({});
   retrieveLocalStorageLists();
-}
 
-function sortList(type) {
-  switch (type) {
-    case 'alpha':
-      items.value.sort()
-      break;
+  /**
+   * Get all the saved lists from local storage and return them as an array of objects
+   * @returns {Array} - Array of objects with the list name and the list
+   */
+  function retrieveLocalStorageLists() {
+    // filter out of local storage all items that start with `list-` and have a length greater than 2 to avoid empty lists
+    // such as, `[]`.
+    const listNames = Object.keys(localStorage).filter(key => key.startsWith('list-') && localStorage.getItem(key).length > 2)
 
-    case 'shuffle':
-      items.value = _shuffle(items.value)
-      break;
+    savedLists.value = listNames.map(name => {
+      const list = localStorage.getItem(name)
+      return {
+        name: name.replace('list-', ''),
+        data: JSON.parse(list)
+      }
+    })
   }
-}
 
-function remove(index) {
-  items.value.splice(index, 1)
-}
-
-/**
- * Loads list data by setting the items to the list items.
- * If the list is empty, it will set the items to the list data.
- * @param list
- * @returns {void}
- */
-function restoreList(list) {
-  if (!!list) {
-    items.value = list.data
+  function inputChange(e) {
+    const keyCode = e.code
+    switch (keyCode) {
+      case 'Enter':
+        if (!!inputVal.value) {
+          items.value.push(inputVal.value.toLowerCase())
+          // items.value.reverse()
+          inputVal.value = ''
+        }
+        break;
+    }
   }
-}
 
-const isListLoaded = computed(() => {
-  return items.value.length > 0
-})
+  function clearList() {
+    items.value = []
+  }
+
+  /**
+   * Save the list items to local storage, prompting the user to give the list a name
+   * @returns {void}
+   */
+  function saveList() {
+    const list = JSON.stringify(items.value)
+    // prompt user with alert to give the list a name
+    const listName = prompt('Please enter a name for the list', 'Team Red')
+    localStorage.setItem(`list-${listName}`, list)
+    retrieveLocalStorageLists();
+  }
+
+  function sortList(type) {
+    switch (type) {
+      case 'alpha':
+        items.value.sort()
+        break;
+
+      case 'shuffle':
+        items.value = _shuffle(items.value)
+        break;
+    }
+  }
+
+  function remove(index) {
+    items.value.splice(index, 1)
+  }
+
+  /**
+   * Loads list data by setting the items to the list items.
+   * If the list is empty, it will set the items to the list data.
+   * @param list
+   * @returns {void}
+   */
+  function restoreList(list) {
+    if (!!list) {
+      items.value = list.data
+    }
+  }
+
+  const isListLoaded = computed(() => {
+    return items.value.length > 0
+  })
 </script>
 
 <style scoped>
-.saved-lists {
-  margin-left: 5px;
-  display: flex;
-  flex-flow: column;
-  align-items: flex-start;
-  gap: 5px;
-  position: absolute;
-  left: 0;
-  top: 0;
-
-  button {
-    margin-left: 10px;
-  }
-}
-
-.controls-wrapper {
-  margin: 0 auto;
-  width: 100%;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  gap: 5px;
-
-  .controls {
+  .saved-lists {
+    margin-left: 5px;
     display: flex;
+    flex-flow: column;
+    align-items: flex-start;
     gap: 5px;
-  }
+    position: absolute;
+    left: 0;
+    top: 0;
 
-  input {
-    border-radius: 5px;
-  }
-
-  button {
-    padding: .5rem;
-    width: fit-content;
-    background: rgba(255, 255, 255, 0.7);
-    font-family: monospace;
-    font-size: .7rem;
-
-    &:active {
-      background: rgba(255, 255, 255, 0.8);
+    button {
+      margin-left: 10px;
     }
   }
 
-  input[type=text] {
-    padding: 8px;
-    width: 60%;
-    background: rgba(255, 255, 255, 0.6);
-    font-family: monospace;
+  .controls-wrapper {
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    gap: 5px;
 
-    &:focus {
-      outline: none;
+    .controls {
+      display: flex;
+      gap: 5px;
+    }
+
+    input {
+      border-radius: 5px;
+    }
+
+    button {
+      padding: .5rem;
+      width: fit-content;
       background: rgba(255, 255, 255, 0.7);
+      font-family: monospace;
+      font-size: .7rem;
+
+      &:active {
+        background: rgba(255, 255, 255, 0.8);
+      }
+    }
+
+    input[type=text] {
+      padding: 8px;
+      width: 60%;
+      background: rgba(255, 255, 255, 0.6);
+      font-family: monospace;
+
+      &:focus {
+        outline: none;
+        background: rgba(255, 255, 255, 0.7);
+      }
     }
   }
-}
 </style>
